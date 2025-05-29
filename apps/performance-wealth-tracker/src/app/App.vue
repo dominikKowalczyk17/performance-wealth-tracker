3a64db8ea70e57ee2090cf28626638f3c83ab377
Dominik <dkowalczyk@archlinux.com>
2025-05-29 21:09:44 +0200

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/Jg1JYwBgkQ

---
+++ apps/performance-wealth-tracker/src/app/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import NxWelcome from './NxWelcome.vue';
+</script>
+
+<template>
+  <NxWelcome title="@performance-wealth-tracker/performance-wealth-tracker" />
+</template>
